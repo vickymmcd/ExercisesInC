8b3b7ddd30e7298e312011ec72f69635dba3d51e
Vicky McDermott <vickymmcd@gmail.com>
2018-01-23 21:47:15 -0500

    finishing up cards program

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -8,36 +8,73 @@
  #include <stdio.h>
  #include <stdlib.h>
 
+ /* Prompts the user for input and puts the reply in the given buffer.
+
+    User input is truncated to the first two characters.
+
+    prompt: string prompt to display
+    card_name: buffer where result is stored
+ */
+ void get_card_name(char *prompt, char *card_name){
+   puts(prompt);
+   scanf("%2s", card_name);
+ }
+
+ /* Gets the card value value based on the card name.
+
+    card_name: name of the card inputted
+    my_default: default value for when it is a number
+ */
+ int get_card_val(char card_name, int my_default){
+   int val = 0;
+   switch(card_name){
+     case 'K':
+     case 'Q':
+     case 'J':
+       val = 10;
+       break;
+     case 'A':
+       val = 11;
+       break;
+     case 'X':
+       break;
+     default:
+       val = my_default;
+       if (val < 1 || val > 10){
+         puts("Error! Bad value!");
+         break;
+       }
+   }
+   return val;
+ }
+
+ /* Updates the count depending on the value.
+
+    val: the value to check and use to update count
+    count: the current count value
+ */
+ int update_count(int val, int count){
+   if (val >= 3 && val <= 6)
+     count++;
+   else if (val == 10)
+     count--;
+   return count;
+ }
+
+ /* Main function to evaluate face card values.
+ */
  int main()
  {
    char card_name[3];
    int count = 0;
+   int val;
    do{
-     puts("Enter the card_name: ");
-     scanf("%2s", card_name);
-     int val = 0;
-     switch(card_name[0]){
-       case 'K':
-       case 'Q':
-       case 'J':
-         val = 10;
-         break;
-       case 'A':
-         val = 11;
-         break;
-       case 'X':
-         continue;
-       default:
-         val = atoi(card_name);
-         if (val < 1 || val > 10){
-           puts("Error! Bad value!");
-           continue;
-         }
+     get_card_name("Enter the card name: ", card_name);
+     val = get_card_val(card_name[0], atoi(card_name));
+     if (val == 0){
+       continue;
      }
-     if (val >= 3 && val <= 6)
-       count++;
-     else if (val == 10)
-       count--;
+     count = update_count(val, count);
      printf("Current count: %i\n", count);
    } while(card_name[0] != 'X');
    return 0;
