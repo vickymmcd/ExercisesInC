1b9dbe1f71b36fc4a1be030cb47f6b043fc05f68
Vicky McDermott <vickymmcd@gmail.com>
2018-02-24 11:27:53 -0500

    adding tests

---
+++ exercises/ex04/trout/test_util.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "util.h"
+
+static char *test1() {
+    char *res = icmpcode_v4(1);
+    char *message = "test1 failed: code 1 should return host unreachable";
+    mu_assert(message, res == "host unreachable");
+    return NULL;
+}
+
+static char *test2() {
+    char *res = icmpcode_v4(5);
+    char *message = "test2 failed: code 5 should return source route failed";
+    mu_assert(message, res == "source route failed");
+    return NULL;
+}
+
+static char * all_tests() {
+    mu_run_test(test1);
+    mu_run_test(test2);
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+    char *result = all_tests();
+    if (result != NULL) {
+        printf("%s\n", result);
+    } else {
+        printf("ALL TESTS PASSED\n");
+    }
+
+    return result != 0;
+}
