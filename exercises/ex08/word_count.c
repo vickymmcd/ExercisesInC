f32ca696a83abea2fa491e9e6d826e7ced95e5bb
Vicky McDermott <vickymmcd@gmail.com>
2018-04-02 16:43:19 -0400

    done with exercise 8

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -6,53 +6,28 @@
 #include <stdlib.h>
 #include <glib.h>
 
-
-gchar* get_contents (void) {
-    const gchar *filename = "prideandprejudice.txt";
-    gchar *contents;
-    gsize len;
-    GError *error = NULL;
-
-    if (! g_file_get_contents (filename, &contents, &len, &error))
-        g_error ("g_file_get_contents() failed: %s", error->message);
-    return contents;
-
-}
-
+// function to print the hash table entries
 void print_hash(gpointer key, gpointer value, gpointer user_data){
     char *pkey = key;
-    printf("%s - %i\n", pkey, GPOINTER_TO_INT(value));
+    printf("%s: %i\n", pkey, GPOINTER_TO_INT(value));
 }
 
 int main(int argc, char** argv) {
-  /*
-  gchar* book = get_contents();
-  //gchar** words = g_strsplit(" ", book, -1);
-  gchar** words = g_strsplit(" ", "hello I am Vicky who are you", -1);
-  //GArray* a = g_array_new(FALSE, FALSE, sizeof(char*));
-  int count = 0;
-  int i;
-  FILE *ptr_file;
-  //printf("%ld", sizeof(words));
-  printf("%s\n", words[0]);
-
-  for(i=0; i<sizeof(words); i++){
-    //printf("%s\n", words[i]);
-  }
-  */
 
   // open up the file and get the file descriptor
   FILE *ptr_file;
-  ptr_file = fopen("prideandprejudice.txt", "r");
+  ptr_file = fopen("words.txt", "r");
   gint file_descriptor = fileno(ptr_file);
   // create a gscanner to read the file
   GScanner *gs = g_scanner_new(NULL);
   // make a ghashtable for the word frequency table
   GHashTable *ght = g_hash_table_new(g_str_hash, g_str_equal);
 
-  //open("prideandprejudice.txt", O_RDONLY);
+  // set up the scanner input file
   g_scanner_input_file(gs, file_descriptor);
+  // read words until the end of the file
   while(g_scanner_eof(gs) == FALSE){
+    // add the key/value to the hash table each time you scan a new word
     g_scanner_get_next_token(gs);
     if(gs->value.v_string == NULL)
       continue;
@@ -62,39 +37,10 @@ int main(int argc, char** argv) {
     else{
       g_hash_table_insert(ght, g_strdup(gs->value.v_string), g_hash_table_lookup(ght, gs->value.v_string)+1);
     }
-    printf("%s\n", gs->value.v_string);
   }
 
+  // print out the values in the hash table
   g_hash_table_foreach(ght, print_hash, NULL);
 
-  //printf("%s", book);
-  //char buf[1000];
-
-  //for(i=0; i<a->len; i++){
-    //printf("%s", book);
-    //printf("%s", g_array_index(a, char*, 0));
-  //}
-/*
-  char **lines;
-  lines = (char**)malloc(sizeof(char)*count);
-  lines[0] = (char*)malloc(sizeof(char)*count*1000);
-  int curcount = 0;
-
-  printf("hello i made it here");
-
-  while (fgets(lines[curcount],1000, ptr_file)!=NULL){
-    curcount++;
-  }
-
-  printf("hiya...");
-
-  for(i=0; i<count; i++){
-    printf("%s", lines[i]);
-    printf("%d", i);
-  }
-
-  printf("count %d\n", count);
-*/
-  //fclose(ptr_file);
   return 0;
 }
